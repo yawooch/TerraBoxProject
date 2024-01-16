59b700a4c669832b2f7b4a275f2e4b9135937447
DESKTOP-N8JQ47C\qortj <qortjddus159@naver.com>
2024-01-17 02:30:39 +0900

    성연 자주묻는질문 01170230

---
+++ Semi_TeraBox/src/main/webapp/views/question/js/qnq.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $('#qt_awn_by').hide();
+    $('#qt_on_by').click(function () {
+        $('#qt_awn_by').toggle();
+    })
+})
+
+$(document).ready(function () {
+    $('#qt_awn_by').show();
+    $('#qt_on_by1').click(function () {
+        $('#qt_awn_by1').toggle();
+    })
+})
