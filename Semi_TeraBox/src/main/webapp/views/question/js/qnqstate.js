3b37522d6e3b4fbf0535e97c5da34dd1e3654ed8
seongyeon <qortjddus159@naver.com>
2024-01-17 19:45:36 +0900

    성연 고객샌터 홈/자묻질/공지사항 중간커밋

--- Semi_TeraBox/src/main/webapp/views/question/js/qnqstate.js
+++ Semi_TeraBox/src/main/webapp/views/question/js/qnqstate.js
@@ -6,58 +6,58 @@ $(document).ready(function () {
 })
 
 $(document).ready(function () {
-    $('#qt_awn_by').show();
+    $('#qt_awn_by').hide();
     $('#qt_on_by1').click(function () {
         $('#qt_awn_by1').toggle();
     })
 })
 
 $(document).ready(function () {
-    $('#qt_awn_by').show();
+    $('#qt_awn_by').hide();
     $('#qt_on_by2').click(function () {
         $('#qt_awn_by2').toggle();
     })
 })
 
 $(document).ready(function () {
-    $('#qt_awn_by').show();
+    $('#qt_awn_by').hide();
     $('#qt_on_by3').click(function () {
         $('#qt_awn_by3').toggle();
     })
 })
 
 $(document).ready(function () {
-    $('#qt_awn_by').show();
+    $('#qt_awn_by').hide();
     $('#qt_on_by4').click(function () {
         $('#qt_awn_by4').toggle();
     })
 })
 $(document).ready(function () {
-    $('#qt_awn_by').show();
+    $('#qt_awn_by').hide();
     $('#qt_on_by5').click(function () {
         $('#qt_awn_by5').toggle();
     })
 })
 $(document).ready(function () {
-    $('#qt_awn_by').show();
+    $('#qt_awn_by').hide();
     $('#qt_on_by6').click(function () {
         $('#qt_awn_by6').toggle();
     })
 })
 $(document).ready(function () {
-    $('#qt_awn_by').show();
+    $('#qt_awn_by').hide();
     $('#qt_on_by7').click(function () {
         $('#qt_awn_by7').toggle();
     })
 })
 $(document).ready(function () {
-    $('#qt_awn_by').show();
+    $('#qt_awn_by').hide();
     $('#qt_on_by8').click(function () {
         $('#qt_awn_by8').toggle();
     })
 })
 $(document).ready(function () {
-    $('#qt_awn_by').show();
+    $('#qt_awn_by').hide();
     $('#qt_on_by9').click(function () {
         $('#qt_awn_by9').toggle();
     })
