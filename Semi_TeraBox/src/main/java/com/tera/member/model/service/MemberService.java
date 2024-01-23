c4d156dba3ca23db11fa03d96af9076c0917e651
DESKTOP-3DG4IG5\user1 <yoc0421@naver.com>
2024-01-23 14:35:38 +0900

    로그인 서비스 추가

--- Semi_TeraBox/src/main/java/com/tera/member/model/service/MemberService.java
+++ Semi_TeraBox/src/main/java/com/tera/member/model/service/MemberService.java
@@ -1,6 +1,7 @@
 package com.tera.member.model.service;
 
 import static com.tera.common.jdbc.JDBCTemplate.commit;
+import static com.tera.common.jdbc.JDBCTemplate.close;
 import static com.tera.common.jdbc.JDBCTemplate.getConnection;
 import static com.tera.common.jdbc.JDBCTemplate.rollback;
 
@@ -11,6 +12,20 @@ import com.tera.member.model.vo.Member;
 
 public class MemberService {
 
+    /** 중복방지를 위해 메소드 생성 */
+    public Member findMemberById(String userId) {
+        
+        Member member = null;
+        
+        Connection connection = getConnection();
+        
+        member = new MemberDao().findMemberById(connection, userId);
+        
+        close(connection);
+        
+        return member;
+    }
+	
 	public int save(Member member) {
 		int result = 0;
 		
@@ -26,8 +41,18 @@ public class MemberService {
 		return result;
 	}
 
-	public Member login(String userId, String userPwd) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    /** 입력한 정보로 회원 정보를 불러온다 */
+    public Member login(String userId, String userPwd)
+    {
+        Member member = this.findMemberById(userId);
+        
+        if(member!=null && member.getPassword().equals(userPwd)){
+            
+            return member;
+        }
+        else {
+            return null;
+        }
+    }
+
 }
