c3a518f5507d2ce067f972a99562dbc10cab7a1e
DESKTOP-3DG4IG5\user1 <yoc0421@naver.com>
2024-01-23 22:04:00 +0900

    티켓 커밋

--- Semi_TeraBox/src/main/java/com/tera/ticket/controller/TicketReserveServlet.java
+++ Semi_TeraBox/src/main/java/com/tera/ticket/controller/TicketReserveServlet.java
@@ -1,7 +1,6 @@
 package com.tera.ticket.controller;
 
 import java.io.IOException;
-import java.sql.Connection;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,11 +8,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.tera.common.jdbc.test.vo.Ticket;
-import com.tera.ticket.model.service.TicketService;
-
 /**
- * 예매 화면으로 가는 페이지를 호출하는 Servlet
+ * 예매 화면 좌석선택으로 가는 페이지를 호출하는 Servlet
  */
 @WebServlet(name = "ticketReserve", urlPatterns = { "/ticket" })
 public class TicketReserveServlet extends HttpServlet {
@@ -22,18 +18,8 @@ public class TicketReserveServlet extends HttpServlet {
     public TicketReserveServlet() {}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String seatNo = request.getParameter("seatNo");
 		
-		Ticket ticket = new Ticket();
-		 
-		int result = new TicketService().save(seatNo); 
-		
-		
-	
-		//일단은 두번째 화면 
-		request.getRequestDispatcher("/views/ticket/bookingSeat2.jsp").forward(request, response);
+		request.getRequestDispatcher("/views/ticket/tk.jsp").forward(request, response);
 		
 	}
-
-	
 }
